42d2f094ef3cd565b19c11695ea39440ad49e016
xymz <xym@lunant.net>
2013-08-26 11:32:09 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-S3',
-    version='0.1.4',
+    version='0.1.5',
     url='https://github.com/xymz/flask-s3.git',
     license='WTFPL',
     author='xymz',
