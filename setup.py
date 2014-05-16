5237f8ae8302ffcc66ba86a94da9cf799412ea7d
xymz <xym@lunant.net>
2014-05-16 16:14:38 +0900

    Added maintainer information and fixed desc.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-Flask-S3
+Flask-S3-gzip
 -------------
 
 Easily serve your static files from Amazon S3.
@@ -13,8 +13,10 @@ setup(
     url='http://github.com/spoqa/flask-s3',
     license='MIT License',
     author='xymz',
+    maintainer='Spoqa',
     author_email='xym@spoqa.com',
-    description='Seamlessly serve the static files of your Flask app from Amazon S3',
+    maintainer_email='dev@spoqa.com',
+    description='Seamlessly serve the static files of your Flask app from Amazon S3 (forked from original flask-s3)',
     long_description=__doc__,
     py_modules=['flask_s3'],
     zip_safe=False,
